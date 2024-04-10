545e6a95f24a5b47ec64918a88652dca1be92d91
Murasame <me@murasame.moe>
2024-04-11 06:45:23 +0800

    ADD: new transact method

--- ovsdb/src/protocol/method/transact.rs
+++ ovsdb/src/protocol/method/transact.rs
@@ -14,7 +14,92 @@ pub enum Operation {
         /// A collection of clauses to act as filters against the table data.
         #[serde(rename = "where")]
         clauses: Vec<String>,
+        /// Which columns you want to select
+        columns: Vec<String>,
     },
+    /// `insert` operation
+    #[serde(rename = "insert")]
+    Insert {
+        /// The [Table][crate::schema::Table] to operate against.
+        table: String,
+        /// The row you want to insert.
+        row: String,
+        /// UUID
+        #[serde(rename = "uuid-name")]
+        uuid_name: Option<String>,
+    },
+    /// `update` operation
+    #[serde(rename = "update")]
+    Update {
+        /// The [Table][crate::schema::Table] to operate against.
+        table: String,
+        /// Condition
+        #[serde(rename = "where")]
+        clauses: Vec<String>,
+        /// The row you want to update.
+        row: Option<String>,
+    },
+    /// `mutate` operation
+    #[serde(rename = "mutate")]
+    Mutate {
+        /// The [Table][crate::schema::Table] to operate against.
+        table: String,
+        /// Condition
+        #[serde(rename = "where")]
+        clauses: Vec<String>,
+        /// The edit you want to apply.
+        mutations: Option<String>,
+    },
+    /// `delete` operation
+    #[serde(rename = "delete")]
+    Delete{
+        /// The [Table][crate::schema::Table] to operate against.
+        table: String,
+        /// A collection of clauses to act as filters against the table data.
+        #[serde(rename = "where")]
+        clauses: Vec<String>,
+    },
+        /// `wait` operation
+    #[serde(rename = "wait")]
+    Wait{
+        /// the Timeout
+        timeout: Option<u64>,
+        /// The [Table][crate::schema::Table] to operate against.
+        table: String,
+        /// A collection of clauses to act as filters against the table data.
+        #[serde(rename = "where")]
+        clauses: Vec<String>,
+        /// The columns you want to wait
+        columns: Vec<String>,
+        /// `!=`or`==`
+        until: String,
+        ///c the rows you want to wait
+        rows: Vec<String>
+    },
+    /// `commit` operation
+    #[serde(rename = "commit")]
+    Commit{
+        /// if its true, all transaction will be store to disk/durable, then return.
+        durable: bool
+    },
+    /// `abort` operation
+    #[serde(rename = "abort")]
+    Abort{
+
+    },
+    /// `comment` operation
+    #[serde(rename = "comment")]
+    Comment{
+        /// comment a transaction
+        comment: String
+    },
+    /// `assert` operation
+    #[serde(rename = "assert")]
+    Assert{
+        /// will be lock if clients does not own the lock;
+        lock: String
+
+    }
 }
 
 /// Parameters for the `transact` OVSDB method.
